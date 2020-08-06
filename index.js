6fe39c32ed094a8837eb7e0aae55b857584b8503
Parker Williamson <parker.williamson@gmail.com>
2020-08-06 16:18:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function handleClick(e) {
 function displayMessage(msg) { 
   document.getElementById("greeting").innerText = msg; 
 }
+
+function greet(timeStr) { 
+  
+  const hour = parseInt(timeStr, 10);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
